1880c5e2c0260e90bbd2614c0c44481190c35614
akaessens <24660231+akaessens@users.noreply.github.com>
2023-03-01 21:14:27 +0100

    first rough implementation finished

--- app/src/main/java/com/akdev/adbmediaremote/ConnectionThread.java
+++ app/src/main/java/com/akdev/adbmediaremote/ConnectionThread.java
@@ -3,10 +3,8 @@ package com.akdev.adbmediaremote;
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 import android.content.SharedPreferences;
-import android.net.TrafficStats;
 import android.util.Log;
 
-import com.tananaev.adblib.AdbBase64;
 import com.tananaev.adblib.AdbConnection;
 import com.tananaev.adblib.AdbCrypto;
 import com.tananaev.adblib.AdbStream;
@@ -21,9 +19,11 @@ import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.util.Base64;
+import java.util.function.Consumer;
 
 /**
- * Thread to send an ADB command to host:port
+ * Thread to send an ADB command to host:port.
+ * If a callback is given, the response will be passed back
  */
 public class ConnectionThread implements Runnable{
     MainActivity ref;
@@ -31,13 +31,15 @@ public class ConnectionThread implements Runnable{
     String host;
     int port;
 
-    String response = "";
+    Consumer<String> callback;
+    String response;
 
-    ConnectionThread(MainActivity ref, String command, String host, int port) {
+    ConnectionThread(MainActivity ref, String command, String host, int port, Consumer<String> callback) {
         this.ref = ref;
         this.command = command;
         this.host = host;
         this.port = port;
+        this.callback = callback;
     }
 
     @Override
@@ -53,18 +55,24 @@ public class ConnectionThread implements Runnable{
             AdbConnection connection = AdbConnection.create(socket, crypto);
             connection.connect();
 
+            Log.d("ConnectionThread", host + ":" + port + " $ "+command);
             AdbStream stream = connection.open("shell:"+command);
-            Thread.sleep(1000);
-            byte[] response_byte = stream.read();
-            this.response = new String(response_byte, StandardCharsets.UTF_8);
 
-            Log.d("ConnectionThread", "Response:" + response);
+            if (callback != null) {
+                byte[] response_byte = stream.read();
+                this.response = new String(response_byte, StandardCharsets.UTF_8);
+                Log.d("ConnectionThread", "Response:" + response);
+                callback.accept(response);
+            }
 
+            Thread.sleep(5000); // always wait before closing again
             connection.close();
             socket.close();
 
+            Log.d("ConnectionThread", "exit");
+
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
--- app/src/main/java/com/akdev/adbmediaremote/MainActivity.java
+++ app/src/main/java/com/akdev/adbmediaremote/MainActivity.java
@@ -3,10 +3,16 @@ package com.akdev.adbmediaremote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +21,118 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(v -> {
-            ExecutorService executor = Executors.newSingleThreadExecutor();
-            executor.execute(new ConnectionThread(
-                    this,
-                    "media volume --adj lower",
-                    "192.168.1.232",
-                    5555)
-            );
+        final ImageButton image_button_prev = findViewById(R.id.image_button_prev);
+        final ImageButton image_button_rew = findViewById(R.id.image_button_rew);
+        final ImageButton image_button_pause = findViewById(R.id.image_button_pause);
+        final ImageButton image_button_ff = findViewById(R.id.image_button_ff);
+        final ImageButton image_button_next = findViewById(R.id.image_button_next);
+        final ImageButton image_button_conn = findViewById(R.id.image_button_conn);
+
+        final SeekBar seekbar = findViewById(R.id.seekbar);
+
+        image_button_prev.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "media dispatch previous",
+                    getHost(), getPort(), null));
+            t.start();
         });
+        image_button_rew.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "media dispatch rewind",
+                    getHost(), getPort(), null));
+            t.start();
+        });
+        image_button_pause.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "media dispatch play-pause",
+                    getHost(), getPort(), null));
+            t.start();
+        });
+        image_button_ff.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "media dispatch fast-forward",
+                    getHost(), getPort(), null));
+            t.start();
+        });
+        image_button_next.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "media dispatch next",
+                    getHost(), getPort(), null));
+            t.start();
+
+        });
+        image_button_conn.setOnClickListener(v -> {
+            Thread t = new Thread(new ConnectionThread(this,
+                    "getprop ro.product.name",
+                    getHost(), getPort(), this::connCb));
+            t.start();
+
+            Thread t2 = new Thread(new ConnectionThread(this,
+                    "media volume --get | tail -n1",
+                    getHost(), getPort(), this::volCb));
+            t2.start();
+        });
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekbar) {
+                Thread t = new Thread(new ConnectionThread(MainActivity.this,
+                        "media volume --set " + seekbar.getProgress(),
+                        getHost(), getPort(), null));
+                t.start();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekbar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
+            }
+        });
+    }
+
+    String getHost()
+    {
+        final EditText edit_text_host = findViewById(R.id.edit_text_host);
+        return edit_text_host.getText().toString();
+    }
+    int getPort()
+    {
+        final EditText edit_text_port = findViewById(R.id.edit_text_port);
+        return Integer.parseInt(edit_text_port.getText().toString());
+    }
+
+    void connCb(String response)
+    {
+        Log.d("callback", "response:" + response);
+        String text = "connected to\n" + response;
+
+        runOnUiThread(() -> Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show());
+    }
+
+    void volCb(String response)
+    {
+        Log.d("callback", "response:" + response);
+
+        Pattern pattern = Pattern.compile("(\\d+)");
+        Matcher matcher = pattern.matcher(response);
+        List<Integer> groups = new ArrayList<>();
+        while (matcher.find()) {
+            groups.add(Integer.parseInt(matcher.group()));
+        }
+
+        if (groups.size() != 3)
+        {
+            Log.w("callback", "could not parse response");
+            return;
+        }
+        int min = groups.get(1);
+        int max = groups.get(2);
+        int val = groups.get(0);
+
+        final SeekBar seekbar = findViewById(R.id.seekbar);
+        seekbar.setMin(min);
+        seekbar.setMax(max);
+        seekbar.setProgress(val);
     }
 }
